bb1c69d71e911d62dec6b31df3af9459acc99701
kieran baptiste <baptiste.kieran.sio@gmail.com>
2026-02-11 10:53:15 +0100

    Ajout du JavaScript

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Animation au chargement
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.style.opacity = "1";
+});
+
+// Message dynamique
+function showMessage() {
+    alert("Merci de visiter mon portfolio !");
+}
